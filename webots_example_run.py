080e915263e8b84f2218bb80f2e96d1ac44b721a
Suke0811 <49264928+Suke0811@users.noreply.github.com>
2022-08-11 13:20:33 -0700

    bypass gpu detection errors in Windows

--- webots_example_run.py
+++ webots_example_run.py
@@ -14,12 +14,12 @@ import ray
 
 logging.basicConfig(level=logging.INFO)
 
-ray.init(local_mode=True)
+ray.init(local_mode=True, num_gpus=1)
 s = Simulation()    # Create instance of Robot testing system
 
 # Create instance of inputs system.
 # You can only have one type of inputs per test
-i = FileInput('out/Pioneer 3-DX_08_10_2022_13_26_54.csv', loop=False)
+#i = FileInput('out/Pioneer 3-DX_08_10_2022_13_26_54.csv', loop=False)
 i = KeyboardInput()
 #i = JoystickInput()
 
@@ -33,10 +33,10 @@ webots_csv = FileOutput(out_dir+'webots'+time_str()+'.csv')      # save to test.
 
 # add robots to simulation
 #robot_ref = s.add_robot(ScalerManipulator, (ScalerHard, '/dev/MOTOR_0', 2), arm2_csv)
-#s.add_robot(CreateDef, WebotsBinder)
+s.add_robot(CreateDef, WebotsBinder)
 #s.add_robot(EpuckDef, WebotsBinder)
 
-s.add_robot(Pioneer3DxDef, WebotsBinder)
+#s.add_robot(Pioneer3DxDef, WebotsBinder)
 #s.add_robot(Pioneer3AtDef, WebotsBinder)
 
 s.run(SimConfig(max_duration=100, dt=0.01, realtime=True, start_time=0, run_speed=1))  # run 10sec, at the end of run, automatically do outputs.
