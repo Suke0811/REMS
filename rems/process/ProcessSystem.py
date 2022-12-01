147c4b05d0ad8191aa4a77d3607a558136fa3157
Suke0811 <yusuketanaka@g.ucla.edu>
2022-11-30 19:31:32 -0800

    added init to ProcessSystem.py

--- rems/process/ProcessSystem.py
+++ rems/process/ProcessSystem.py
@@ -3,17 +3,23 @@ from rems.sim_handler.job_background.job_type import job_type
 
 
 class ProcessSystem:
-    def process(self, t):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def init(self, *args, **kwargs):
+        pass
+
+    def process(self, t, *args, **kwargs):
         """Called every timestep
         :return job_type, job to submit"""
         return [job_type(self.job)]
 
-    def job(self, **kwargs):
+    def job(self, *args, **kwargs):
         """a job that is done in background
         :return job_return_type: instance to self.done and **kwargs """
         return job_return_type(self.done, **kwargs)
 
-    def done(self, **kwargs):
+    def done(self, *args, **kwargs):
         """called when the job is done"""
 
     def process_callback(self, *args, **kwargs):
